bc4a427ba514495b356180788c46b1276b2cc4f9
eugene <1160178908@qq.com>
2019-04-04 19:39:49 +0800

    0404

--- blog/routes/admin.js
+++ blog/routes/admin.js
@@ -19,15 +19,64 @@ router.get("/",(req,res)=>{
 	})
 })
 
-
+//显示用户列表
 router.get("/users",(req,res)=>{
-	UserModel.find({},'-password -__v')
-	.then(users=>{
-		res.render('admin/user_list',{
-			userInfo:req.userInfo,
-			users
-		})
-	})	
+	/*
+	分页：
+	约定：每页显示两条数据limit(2) limit = 0
+
+	第一页 跳过0条 skip(0)
+	第二页 跳过2条 skip(2)
+	第三页 跳过4条 skip(4)
+
+	第page页 跳过 (page - 1) * limit 条 skip((page - 1) * limit)
+	*/
+
+
+
+	let { page } = req.query;
+	//每页显示条数
+	const limit = 2;
+
+	page = parseInt(page)
+
+	if(isNaN(page)){
+		page = 1;
+	}
+
+	if(page == 0){
+		page = 1;
+	}
+
+	UserModel.countDocuments({})
+	.then(count=>{
+		//计算总页数
+		const pages = Math.ceil(count / limit)
+		if(page > pages){
+			page = pages
+		}
+		//生成页码数组
+		const list = [];
+		for(let i = 1;i<=pages;i++){
+			list.push(i)
+		}
+		//每页显示条数
+		const skip = (page - 1) * limit
+
+		UserModel.find({},'-password -__v')
+		.skip(skip)
+		.limit(limit)
+		.then(users=>{
+			res.render('admin/user_list',{
+				userInfo:req.userInfo,
+				users,
+				page,
+				list
+			})
+		})	
+	})
+
+	
 })
 
 module.exports = router
