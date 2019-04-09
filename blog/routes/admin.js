bea6d481045bb51c441b4741624f4cf9dd827bf4
eugene <1160178908@qq.com>
2019-04-09 18:04:37 +0800

    0409 pm

--- blog/routes/admin.js
+++ blog/routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const UserModel = require('../models/user.js')
+const pagination = require('../util/pagination.js')
 const router = express.Router()
 
 //权限验证
@@ -33,7 +34,7 @@ router.get("/users",(req,res)=>{
 	*/
 
 
-
+/*
 	let { page } = req.query;
 	//每页显示条数
 	const limit = 2;
@@ -75,7 +76,25 @@ router.get("/users",(req,res)=>{
 			})
 		})	
 	})
-
+*/
+	const options = {
+		page:req.query.page,
+		model:UserModel,
+		query:{},
+		projection:'-password -__v',
+		sort:{_id:1}
+	}
+	pagination(options)
+	.then(data=>{
+		res.render('admin/user_list',{
+			userInfo:req.userInfo,
+			users:data.docs,
+			page:data.page,
+			list:data.list,
+			pages:data.pages,
+			url:'/admin/users'
+		})
+	})
 	
 })
 
--- blog/util/pagination.js
+++ blog/util/pagination.js
@@ -1,26 +1,24 @@
 /*
 page:请求页码
-model：数据类型
-query：查询条件
-projection：投影
-sort：排序
+model:数据模型
+query:查询条件
+projection:投影
+sort:排序
+populates:关联的数组
 */
 async function pagination(options){
 	/*
-	分页：
-	约定：每页显示两条数据limit(2) limit = 0
+	分页:
+	约定:每一页显示 2 条 limit(2) limit = 2
+	
+	第 1 页 跳过 0 条 skip(0)
+	第 2 页 跳过 2 条 skip(2)
+	第 3 页 跳过 4 条 skip(4)
 
-	第一页 跳过0条 skip(0)
-	第二页 跳过2条 skip(2)
-	第三页 跳过4条 skip(4)
+	第 page 页 跳过 （page -1）* limit 条 skip(（page -1）* limit)
 
-	第page页 跳过 (page - 1) * limit 条 skip((page - 1) * limit)
 	*/
-
-
-
-	let { page,model,query,projection,sort } = options;
-	//每页显示条数
+	let { page,model,query,projection,sort,populates } = options;
 	const limit = 2;
 
 	page = parseInt(page)
@@ -29,32 +27,49 @@ async function pagination(options){
 		page = 1;
 	}
 
-	if(page == 0){
+	if(page ==0){
 		page = 1;
 	}
 
-	const count = await model.countDocuments(query)
+	const count = await model.countDocuments(query);
 
 	//计算总页数
 	const pages = Math.ceil(count / limit)
+
 	if(page > pages){
 		page = pages
 	}
+	//如果总页数是0
+	if(pages == 0){
+		page = 1;
+	}
+	
 	//生成页码数组
 	const list = [];
 	for(let i = 1;i<=pages;i++){
 		list.push(i)
 	}
-	//每页显示条数
-	const skip = (page - 1) * limit
 
-	const docs = await model.find(query,projection).sort(sort).skip(skip).limit(limit)
+	//跳过条数
+	const skip = (page -1) * limit
+
+	let result = model.find(query,projection);
+	if(populates){
+		populates.forEach(populate=>{
+			result = result.populate(populate);
+		})
+	}
+
+	const docs = await result.sort(sort).skip(skip).limit(limit)
 
 	return {
 		docs,
 		page,
-		list
+		list,
+		pages		
 	}
+
 }
+
 
 module.exports = pagination
